9fba2ae852c48e7de2cac560c7901589dfb556ad
Bruno Sapienza <bruno.sapienza@gmail.com>
2019-10-03 10:12:28 +1000

    create the people table

---
+++ app/models/person.rb
@@ -0,0 +1,11 @@
+class Person < ApplicationRecord
+  has_many :locations_people, class_name: 'LocationsPeople'
+  has_many :affiliations_people, class_name: 'AffiliationsPeople'
+
+  has_many :locations, through: :locations_people
+  has_many :affiliations, through: :affiliations_people
+
+  validates :first_name, presence: true
+  validates :gender, presence: true
+  validates :species, presence: true
+end
