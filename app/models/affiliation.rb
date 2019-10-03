9f703770db37c6cb9099dfed97911f857d4bb294
Bruno Sapienza <bruno.sapienza@gmail.com>
2019-10-03 10:13:03 +1000

    create the affiliations table

---
+++ app/models/affiliation.rb
@@ -0,0 +1,6 @@
+class Affiliation < ApplicationRecord
+  has_many :affiliations_people, class_name: 'AffiliationsPeople'
+  has_many :people, through: :affiliations_people
+
+  validates :name, presence: true
+end
