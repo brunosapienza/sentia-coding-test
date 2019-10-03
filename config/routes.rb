8756254eb7e7537e7c1b7e47ac61c49ddf7bf1c6
Bruno Sapienza <bruno.sapienza@gmail.com>
2019-10-03 10:53:37 +1000

    generate the import controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'import/index'
+  root 'import#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
