02c9b3a3f71ab1615fbb0c5a0b393378a5b44a95
Bruno Sapienza <bruno.sapienza@gmail.com>
2019-10-04 13:53:20 +1000

    add table sorting

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("bootstrap-table")
 
 import './bootstrap_custom.js'
 import '../stylesheets/application'
